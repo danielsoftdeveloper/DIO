41642ae3d97a10f6850a0eaea25a2080ac8a6b85
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-11-25 12:15:08 -0300

    created function jump

--- dino-game/script.js
+++ dino-game/script.js
@@ -1,9 +1,40 @@
 const dino = document.querySelector('.dino');
 
-function handleKeyUp(event){
-    if(event.keyCode === 32){
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
         console.log("presionou spaço");
+        jump();
     }
+}
+
+function jump() {
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 180) {
+            //para subida
+            clearInterval(upInterval);
+
+            //Descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+
+                } else {
+                    // controle velocidade descida    
+                    position -= 5;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+ 
+        } else {    
+            //subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+
+    }, 20);
+
 }
 
 document.addEventListener('keyup', handleKeyUp);
